2f606fef6b8d73770187f67e0d1e45ec76595a67
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-12-07 20:41:54 +0100

    Link to view from theme list

--- lift/src/main/scala/org/drugis/snippet/ThemeSnippet.scala
+++ lift/src/main/scala/org/drugis/snippet/ThemeSnippet.scala
@@ -40,8 +40,7 @@ class ThemeSnip {
 	}
 
 	def view(form: NodeSeq) = {
-		val id = S.param("id").open_!.toLong
-		val theme = Theme.find(By(Theme.id, id)).open_! ;
+		val theme = currentTheme
 
 		def doBind(form: NodeSeq) =
 			bind("theme", form,
@@ -56,23 +55,31 @@ class ThemeSnip {
 		Theme.findAll(By(Theme.createdBy, User.currentUser),
 			OrderBy(Theme.value, Descending))
 
-	private def title(theme: Theme, reDraw: () => JsCmd) =
+	private def ajaxTitle(theme: Theme, reDraw: () => JsCmd) =
 		swappable(<span>{theme.title}</span>,
 			<span>{ajaxText(theme.title, v => {theme.title(v).save; reDraw()})}
 			</span>)
 
-	private def description(theme: Theme, reDraw: () => JsCmd) =
+	private def ajaxDescription(theme: Theme, reDraw: () => JsCmd) =
 		swappable(<span>{theme.description}</span>,
 			<span>{ajaxText(theme.description, v => {theme.description(v).save; reDraw()})}
 			</span>)
 
+	private def ajaxValue(theme: Theme, reDraw: () => JsCmd) =
+		ajaxSelect(Theme.valueList, Full(theme.value.toString),
+			v => {theme.value(v.toInt).save; reDraw()})
+
 	private def doList(reDraw: () => JsCmd)(html: NodeSeq): NodeSeq =
 		toShow.flatMap(theme => bind("theme", html,
-			"value" -> ajaxSelect(Theme.valueList, Full(theme.value.toString),
-				v => {theme.value(v.toInt).save; reDraw()}),
-			"title" -> title(theme, reDraw),
-			"description" -> description(theme, reDraw)
+			"value" -> ajaxValue(theme, reDraw),
+			"title" -> ajaxTitle(theme, reDraw),
+			"description" -> ajaxDescription(theme, reDraw),
+			"link" -> link("/theme", () => CurrentThemeVar(theme), Text("View"))
 		))
+
+	object CurrentThemeVar extends RequestVar[Theme]({Theme.create.createdBy(User.currentUser)})
+
+	def currentTheme = CurrentThemeVar.is
 
 	def list (html: NodeSeq) = {
 		val id = S.attr("themes_id").open_!
