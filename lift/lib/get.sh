ef6377165dd8f637041d05b97a04aa1d8df9dead
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-12-08 15:28:39 +0100

    webapp: added tools to dump database

---
+++ lift/lib/get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://mirror.hostfuss.com/apache/db/derby/db-derby-10.5.3.0/db-derby-10.5.3.0-lib.zip
+unzip db-derby-10.5.3.0-lib.zip
+
+wget http://mirror.hostfuss.com/apache/db/ddlutils/ddlutils-1.0/binaries/DdlUtils-1.0-bin.zip
+unzip DdlUtils-1.0-bin.zip -d ddlutils-1.0-bin
