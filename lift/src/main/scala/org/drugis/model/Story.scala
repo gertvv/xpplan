86953927a4e9ee174ebac700857cc1a7645d9ea7
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-12-09 16:22:07 +0100

    Implement "NA" values for estimates.

--- lift/src/main/scala/org/drugis/model/Story.scala
+++ lift/src/main/scala/org/drugis/model/Story.scala
@@ -18,22 +18,22 @@ with ManyToMany {
 	object description extends MappedText(this)
 	object createdBy extends MappedLongForeignKey(this, User)
 	object value extends MappedInt(this) {
-		override def defaultValue = 4
+		override def defaultValue = 0
 		override def validations = validValue _ :: super.validations
 
 		def validValue(in: Int): List[FieldError] =
-		if (in > 0 && in <= 5) Nil
+		if (in >= 0 && in <= 5) Nil
 		else List(FieldError(this, <b>Value must be in {Story.valueList.toString}</b>))
 
 		override def _toForm = Full(select(Story.valueList, Full(is.toString),
 			f => set(f.toInt)))
 	}
 	object complexity extends MappedInt(this) {
-		override def defaultValue = 2
+		override def defaultValue = 0
 		override def validations = validComplexity _ :: super.validations
 
 		def validComplexity(in: Int): List[FieldError] =
-		if (Story.complexityList.contains((in.toString, in.toString))) Nil
+		if (in == 0 || Story.complexityList.contains((in.toString, in.toString))) Nil
 		else List(FieldError(this, <b>Complexity must be in {Story.complexityList.toString}</b>))
 
 		override def _toForm = Full(select(Story.complexityList,
@@ -47,8 +47,8 @@ with ManyToMany {
 }
 
 object Story extends Story with LongKeyedMetaMapper[Story] {
-	lazy val valueList = (1 to 5).map(v => (v.toString, v.toString))
-	lazy val complexityList = List(1, 2, 3, 5, 8).map(v => (v.toString, v.toString))
+	lazy val valueList = ("0", "NA") :: ((1 to 5).map(v => (v.toString, v.toString))).toList
+	lazy val complexityList = ("0", "NA") :: List(1, 2, 3, 5, 8).map(v => (v.toString, v.toString)).toList
 	lazy val sitemap : List[Menu] = List(Menu(Loc("Stories", List("stories"), "Stories")))
 	def storyList = findAll().map({story:Story => (story, story.title.toString)})
 }
